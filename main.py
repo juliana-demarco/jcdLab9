f37b626c3a1a23e10df8d86257c9b01bde9f4614
juliana-demarco <166638630+juliana-demarco@users.noreply.github.com>
2024-04-10 16:17:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+def main():
+    while True:
+        print_menu()
+        option = input("Please enter an option: ")
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        if option == "2":
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {decoded_password}, and the original password is {encoded_password}")
+
+
+def encode(password):
+    encoded_password = ""
+    for i in password:
+        i = int(i)
+        i += 3
+        encoded_password += str(i)
+    return encoded_password
+
+
+if __name__ == "__main__":
+    main()
